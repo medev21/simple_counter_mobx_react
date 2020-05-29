8bd38a7a64dc72815902539c719fe344e686a00d
medev21 <benmarteen07@gmail.com>
2020-05-29 12:58:17 -0400

    style apps

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Counter from './components/Counter';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Counter />
     </div>
   );
 }
